27326fd447c2227607dc006fd997490d89af093a
Kaio Silveira <silveira.kaio@icloud.com>
2024-10-20 12:06:39 +0100

    setup initial examples + tests + CI

--- src/index.test.js
+++ src/index.test.js
@@ -1,7 +1,20 @@
-import { toBeRefactored } from './index';
+import { Order } from './index';
 
-describe('functionToBeRefactored', () => {
-  it('should work', () => {
-    expect(toBeRefactored()).toEqual('Hello, world!');
+describe('Order', () => {
+  describe('finalprice', () => {
+    it('should apply discount level 1 if quantity is lower than 100', () => {
+      const order = new Order({ quantity: 10, itemPrice: 10 });
+      expect(order.finalPrice).toBe(95);
+    });
+
+    it('should apply discount level 1 if quantity is equal 100', () => {
+      const order = new Order({ quantity: 100, itemPrice: 10 });
+      expect(order.finalPrice).toBe(950);
+    });
+
+    it('should apply discount level 2 if quantity is higher than 100', () => {
+      const order = new Order({ quantity: 101, itemPrice: 10 });
+      expect(order.finalPrice).toBe(909);
+    });
   });
 });
